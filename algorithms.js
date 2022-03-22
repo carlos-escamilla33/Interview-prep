97761e670682297229e8b593ec5f85e38cb89835
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2022-03-22 11:41:41 -0700

    working on max subarray leetcode#53

--- algorithms.js
+++ algorithms.js
@@ -1,19 +1,28 @@
 // <-------------FlattenArray--------------->
 
-let exampleArray = [1,2,[3,4,5], [4]];
+let exampleArray = [1, 2, [3, 4, 5], [4]];
 // let exampleArray = [1,2,[3,4,5,[6,7]]];
 const flatten = (arr) => {
-    let flatArray = []; // initalize empty array to push non array elements into
-    for (let i = 0; i < arr.length; i++) { // for loop through the array
-        const item = arr[i]; // initialize as item each element in the array
-        if (Array.isArray(item)) { // check to see if the item is an array
-            flatArray = flatArray.concat(flatten(item)); // if it is an array then we use recursion to break it down
-            // we concat our current flat array to the returned value of the flattened array item
-        } else {
-            flatArray.push(item); // if the item is not an array then we simply push it to the flattened array
-        }
+  let flatArray = []; // initalize empty array to push non array elements into
+  for (let i = 0; i < arr.length; i++) { // for loop through the array
+    const item = arr[i]; // initialize as item each element in the array
+    if (Array.isArray(item)) { // check to see if the item is an array
+      flatArray = flatArray.concat(flatten(item)); // if it is an array then we use recursion to break it down
+      // we concat our current flat array to the returned value of the flattened array item
+    } else {
+      flatArray.push(item); // if the item is not an array then we simply push it to the flattened array
     }
-    return flatArray;
+  }
+  return flatArray;
 }
 
 // console.log(flatten(exampleArray));
+
+// <-------------Maximum Subarray LEETCODE #53--------------->
+let arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+
+const maxSubArray = (nums) => {
+  
+}
+
+maxSubArray(arr);
