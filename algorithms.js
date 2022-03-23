b047a312b0a1cd77aea7e92cd9e24ea0933a724c
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2022-03-22 19:43:03 -0700

    completed twoSum leetcode#1

--- algorithms.js
+++ algorithms.js
@@ -1,3 +1,22 @@
+// <-------------TwoSum LeetCode#1--------------->
+const twoSum = (arr, target) => {
+  // create a hashtable to store the nums
+  let nums = {};
+  for (let i = 0; i < arr.length; i++) {  // for loop through the nums
+    const currNum = arr[i]; //initialize the current number we are at
+    const possiblePair = target - currNum; // if you find the difference of the current number and the target
+    if (possiblePair in nums) {  // if the result of target - currNum is not in the hashmap then we keep on iterating
+      return [i, nums[possiblePair]]; // return the number we are at and the result of target - the currNum
+    } else {
+      nums[currNum] = i;
+    }
+  }
+}
+
+console.log(twoSum([3,2,4], 6));
+
+
+
 // <-------------FlattenArray--------------->
 
 let exampleArray = [1, 2, [3, 4, 5], [4]];
@@ -25,4 +44,4 @@ const maxSubArray = (nums) => {
   
 }
 
-maxSubArray(arr);
+// maxSubArray(arr);
