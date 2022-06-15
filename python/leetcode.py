c44bbc3007c3acb6b3e29efe38c2a9613e56503e
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2022-06-14 22:51:51 -0700

    completed leetcode #20

---
+++ python/leetcode.py
@@ -0,0 +1,22 @@
+
+# ----------------Leetcode #20 Valid Parentheses ---------------- #
+
+def isValid(string):
+    bracePairs = {
+        "]": "[", 
+        ")": "(",
+        "}": "{",
+    }
+    stack = []
+
+    for char in string:
+        print(stack)
+        if char in bracePairs and len(stack) > 0:
+            if bracePairs[char] != stack[-1]:
+                return False
+            else:
+                stack.pop()
+        else:
+            stack.append(char)
+
+    return len(stack) == 0
