68ec062ef5430107e6b616fc2c57422c8a76f868
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2022-06-25 00:42:04 -0700

    completed maxProfit leetcode 121

--- python/leetcode.py
+++ python/leetcode.py
@@ -29,7 +29,7 @@ class ListNode:
         self.next = None
 
 def mergeTwoSortedLists(list1, list2):
-    # Create a dummy node
+    # Create a dummy node that just holds None
     node = ListNode()
     # Assign the tail to the node node
     tail = node
@@ -42,7 +42,7 @@ def mergeTwoSortedLists(list1, list2):
             tail.next = list1
             # Update list1 head to the next node
             list1 = list1.next
-        # if list2.val < list1.val
+        # if list2.val less than or equal to list1.val
         # Update next node in tail to be the current node at list2
         elif list2.val <= list1.val:
             tail.next = list2
@@ -61,7 +61,32 @@ def mergeTwoSortedLists(list1, list2):
     # return the head of the next node in the node
     # first one is None
     return node.next
+
+# ----------------Leetcode # 121 Best Time to Buy and Sell Stock ----------------
+
+prices = [7,1,5,3,6,4]
+
+def maxProfit(prices):
+    # Initialize max profit
+    maxProfit = 0
+    # set the min, which is currently the first number
+    currMin = prices[0]
+
+    # Iterate through the indexes of array
+    for i in range(len(prices)):
+        # Initialize the current price index
+        price = prices[i]
+        # Initialize the current profit which is price - currMin
+        currProfit = price - currMin
+        # If the current profit is more than the max profit
+        # assign the current profit to max profit
+        if currProfit > maxProfit:
+            maxProfit = currProfit
+        # if price is less than the min than we assign the price to current min
+        elif price < currMin:
+            currMin = price
     
+    return maxProfit
 
 
 
