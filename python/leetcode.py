cc3c0ffa3b1e7aef60cb211d5e9e560c6cafa65b
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2022-06-24 22:14:24 -0700

    completed Leetcode #21

--- python/leetcode.py
+++ python/leetcode.py
@@ -20,4 +20,48 @@ def isValid(string):
 
     return True if not stack else False
 
-# ----------------Leetcode Merge Two Sorted Lists ---------------- #
+# ----------------Leetcode #21 Merge Two Sorted Lists ---------------- #
+
+# Create list node
+class ListNode:
+    def __init__(self, val=None):
+        self.val = val
+        self.next = None
+
+def mergeTwoSortedLists(list1, list2):
+    # Create a dummy node
+    node = ListNode()
+    # Assign the tail to the node node
+    tail = node
+
+    # While list1 and list2 are not null
+    while list1 != None and list2 != None:
+        # if the value in list1 is greater than the value at list2
+        if list1.val < list2.val:
+            # Update next node in tail to the current node list1
+            tail.next = list1
+            # Update list1 head to the next node
+            list1 = list1.next
+        # if list2.val < list1.val
+        # Update next node in tail to be the current node at list2
+        elif list2.val <= list1.val:
+            tail.next = list2
+            # Update list2 head to the next node
+            list2 = list2.next
+        
+        # Update tail to move on to the next node
+        tail = tail.next
+    
+    # if list1 is still not empty
+    if list1:
+        tail.next = list1
+    elif list2:
+        tail.next = list2
+    
+    # return the head of the next node in the node
+    # first one is None
+    return node.next
+    
+
+
+
