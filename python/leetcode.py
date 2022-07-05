ed1d9c5ab468c4233bb1acd06523b049e47fec07
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2022-07-04 21:02:42 -0700

    updated tail to head in leetcode #21

--- python/leetcode.py
+++ python/leetcode.py
@@ -31,23 +31,23 @@ class ListNode:
 def mergeTwoSortedLists(list1, list2):
 
     node = ListNode()
-    tail = node
+    head = node
 
     while list1 != None and list2 != None:
         if list1.val < list2.val:
-            tail.next = list1
+            head.next = list1
             list1 = list1.next
-        elif list2.val <= list1.val:
-            tail.next = list2
+        else:
+            head.next = list2
             list2 = list2.next
 
-        tail = tail.next
+        head = head.next
     
 
     if list1:
-        tail.next = list1
+        head.next = list1
     elif list2:
-        tail.next = list2
+        head.next = list2
         
     return node.next
 
