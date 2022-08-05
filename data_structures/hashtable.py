681316ca86a32f050568763752414a9e638697ad
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2022-08-04 21:32:58 -0700

    added is_full and double method to hashtable

--- data_structures/hashtable.py
+++ data_structures/hashtable.py
@@ -2,12 +2,15 @@ class Hashtable(object):
     def __init__(self, length=4):
         self.array = [None] * length
     
-    def hash(self, key):
+    def _getHash(self, key):
         length = len(self.array)
-        return hash(key) % length
+        hash = 0
+        for char in str(key):
+            hash+=ord(char)
+        return hash % length
     
     def add(self, key, value):
-        index = self.hash(key)
+        index = self._getHash(key)
         if self.array[index] is not None:
             for key_val in self.array[index]:
                 if key_val[0] == key:
@@ -18,9 +21,12 @@ class Hashtable(object):
         else:
             self.array[index] = []
             self.array[index].append([key, value])
+            
+        if self.is_full():
+            self.double()
     
     def get(self, key):
-        index = self.hash(key)
+        index = self._getHash(key)
         if self.array[index] is None:
             raise KeyError()
         else:
@@ -30,8 +36,26 @@ class Hashtable(object):
             
             raise KeyError()
 
+    def is_full(self):
+        items = 0
+        for item in self.array:
+            if item is not None:
+                items+=1
+        return items > len(self.array) / 2
+
+    def double(self):
+        ht2 = Hashtable(length=len(self.array) * 2)
+        for i in range(len(self.array)):
+            if self.array[i] is None:
+                continue
+
+            for key_val in self.array[i]:
+                ht2.add(key_val[0], key_val[1])
+        
+        self.array = ht2.array
+
 ht = Hashtable()
 
 ht.add(1, "carlos")
 
-print(ht.get(1))
+print(ht.is_full())
