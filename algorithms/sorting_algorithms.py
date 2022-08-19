9288034cd028a728623453483fce72691358a293
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2022-08-18 18:11:11 -0700

    updated bubble sort

--- algorithms/sorting_algorithms.py
+++ algorithms/sorting_algorithms.py
@@ -1,13 +1,19 @@
 def bubbleSort(arr):
     length = len(arr) - 1
-    for _ in range(length):
-        for j in range(length):
+    for i in range(length):
+        noSwaps = True
+        for j in range(length - i):
             if arr[j] > arr[j+1]:
                 temp = arr[j]
                 arr[j] = arr[j+1]
                 arr[j+1] = temp
+                noSwaps = False
 
-    print(arr)
+        if noSwaps:
+            return arr
+
+    return arr
 
 
-bubbleSort([32,1,9,6])
+
+print(bubbleSort([32,1,9,6]))
