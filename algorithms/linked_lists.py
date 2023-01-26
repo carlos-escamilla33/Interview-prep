065925ccc73ff68091fd9c68767e9f9913250124
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2023-01-25 21:37:25 -0800

    findByIdxRecurrsive and findByIdxInterative Linked List

--- algorithms/linked_lists.py
+++ algorithms/linked_lists.py
@@ -4,10 +4,10 @@ class Node:
         self.next = next
 
 
-a = Node("A")
-b = Node("B")
-c = Node("C")
-d = Node("D")
+a = Node(4)
+b = Node(10)
+c = Node(3)
+d = Node(40)
 
 a.next = b
 b.next = c
@@ -54,6 +54,72 @@ def linkedListValuesRecurr(head):
 
 # linkedListValues(a)
 
-linkedListValuesRecurr(a)
+# linkedListValuesRecurr(a)
+
+# Add up the nodes in the linked list and return them
+
+def sumLinkedListVals(head):
+    total = 0
+    current = head
+    while current:
+        total+=current.val
+        current = current.next
+    return total
+
+def sumLinkedListValsRecurr(head):
+    if not head:
+        return 0
+    return head.val + sumLinkedListValsRecurr(head.next)
+
+# print(sumLinkedListVals(a))
+# print(sumLinkedListValsRecurr(a))
+
+# Find target value in linked list
+
+# O(N) runtime
+# O(1) memory
+
+def findTarget(head, target):
+    current = head
+    while current:
+        if current.val == target:
+            return True
+        current = current.next
+    return False
+
+def findTargetRecurr(head, target):
+    if not head:
+        return False
+    elif head.val == target:
+        return True
+    return findTargetRecurr(head.next, target)
+    
+
+# print(findTarget(a, 90))
+
+# print(findTargetRecurr(a, 3))
+
+# Find the node by index in the linked list
+
+def findByIdx(head, idx):
+    count = 0
+    current = head
+    while current:
+        if count == idx:
+            return current.val
+        count+=1
+        current = current.next
+    return f"Index {idx} is out of range"
+
+def findByIdxRecurr(head, idx):
+    if not head:
+        return "Idx not in range"
+    elif idx == 0:
+        return head.val
+    idx-=1
+    return findByIdx(head.next, idx)
+
+# print(findByIdx(a, 2))
+# print(findByIdxRecurr(a, 9))
 
 
