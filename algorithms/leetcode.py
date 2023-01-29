8fe841caaa4960094891a79c9b2f782c745ae1b1
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2023-01-29 00:03:50 -0800

    leetcode 21. mergeTwoSortedLists

--- algorithms/leetcode.py
+++ algorithms/leetcode.py
@@ -304,3 +304,24 @@ def majorityElement(nums):
             numOccurrences = freqHt[num]
 
     return majorityNum
+
+# ----------------Leetcode #21 Merge Two Sorted Lists ----------------
+
+def mergeTwoLists(list1, list2):
+    dummyNode = ListNode()
+    tail = dummyNode
+    current1 = list1
+    current2 = list2
+    while current1 and current2:
+        if current1.val < current2.val:
+            tail.next = current1
+            current1 = current1.next
+        else:
+            tail.next = current2
+            current2 = current2.next
+        tail = tail.next
+    if current1:
+        tail.next = current1
+    elif current2:
+        tail.next = current2
+    return dummyNode
