bb2330333d08f1ecc21efbf88a8fca8c76c5670b
Carlos Escamilla <carlosaescamilla3@gmail.com>
2026-02-08 14:09:06 -0800

    leetcode 1657

---
+++ LeetCode_75/hash_map_set/1657.Determine_if_Two_Strings_Are_Close.py
@@ -0,0 +1,22 @@
+def helper(word):
+    ht = {}
+
+    for char in word:
+        if char not in ht:
+            ht[char] = 0
+        ht[char] += 1
+    
+    return ht
+
+def closeStrings(word1, word2):
+    # check the length
+    if len(word1) != len(word2):
+        return False
+    
+    word1_ht = helper(word1)
+    word2_ht = helper(word2)
+
+    # check the characters are the same and the frequency of each character
+    return (word1_ht.keys() == word2_ht.keys() 
+            and sorted(word1_ht.values()) == sorted(word2_ht))
+    
