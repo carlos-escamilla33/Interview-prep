08732f0562ecd69d9913e1d2201703ef7e191852
Carlos Escamilla <carlosaescamilla3@gmail.com>
2026-02-08 19:47:06 -0800

    Leetcode 1768

---
+++ LeetCode_75/array_string/1768.Merge_Strings_Alternatively.py
@@ -0,0 +1,12 @@
+def mergeAlternately(word1, word2):
+    res = []
+    i = 0
+
+    while i < len(word1) or i < len(word2):
+        if i < len(word1):
+            res.append(word1[i])
+        if i < len(word2):
+            res.append(word2[i])
+        i += 1
+    
+    return res
